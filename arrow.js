a5dcbb74591e05d797008c710bac4ff20d15b802
kadir-zeren <k4dzeren@gmail.com>
2024-11-13 09:42:44 +0300

    exponintial

--- arrow.js
+++ arrow.js
@@ -37,3 +37,29 @@ console.log("BIGGEST:", findBiggest(1, 3, 2));
 //? Alternatif olarak
 const findBiggestShort = (num1, num2, num3) => Math.max(num1, num2, num3);
 console.log("BIGGEST:", findBiggestShort(1, 3, 2));
+
+//* ORNEK: Doğum tarihini parametre olarak alan ve ana
+//* programa yaşı hesaplayıp döndüren fonksiyonu yazınız.
+// **************************************** /
+
+const calculateAge = (year) => new Date().getFullYear() - year;
+
+console.log(`AGE : ${calculateAge(1990)}`);
+
+//* ORNEK: Silindir Hacmi
+// ***************************************** /
+//? hacim = Pi x r x r x h
+
+const h = Number(prompt("h:"));
+const r = Number(prompt("r:"));
+// const pi = 3.14
+
+const volume = (radius, height) => Math.PI * radius * radius * height;
+console.log("VOLUME:", volume(r, h).toFixed(2));
+console.log("VOLUME:", volume(r, h).toPrecision(5));
+console.log("VOLUME:", volume(r, h).toExponential(5));
+
+//? toPrecision(5) aldigi parametreye gore bir sayinin tam ve kesir
+//? kisimlarinin ne kadarini gosterilecegini belirlemek icin
+//? kullanılabilir. 5 degeri tam ve kesir icin toplam 5 basamak kullan demektir.
+//? toExponential() ise bir sayinin bilimsel (10 uzeri) olarak
